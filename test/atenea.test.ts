7d7102db46e6c448bc965e0f87b1735630fd7fa2
Leidy Quiñones <lalis.181014@gmail.com>
2023-05-28 22:06:47 -0500

    v0.0.2

--- test/atenea.test.ts
+++ test/atenea.test.ts
@@ -1,4 +1,4 @@
-import {Builder, By, WebDriver} from 'selenium-webdriver'
+import {Builder, By, Key, WebDriver} from 'selenium-webdriver'
 import assert from 'assert'
 
 
@@ -36,6 +36,14 @@ describe(
                 let titulo= await miNavegador.getTitle()
                 assert.match(titulo, /Atenea/i) //Mira si un texto cumple una expresión regular--Busca que en algún lado diga atenea, el i hace que no importe si viene en mayúsculas o minúsculas
                 await miNavegador.sleep(2000)
+
+                //Conectarse a una API y obtener una palabra
+                for(let i=0;i<10; i++){
+                let respuesta=await fetch('https://random-word-api.herokuapp.com/word?number=10')
+                let palabra=await respuesta.json()
+                let miBusqueda= await miNavegador.findElement(By.id('edit-keys'))
+                await miBusqueda.sendKeys(palabra[0],Key.RETURN) //Pone la palabra ejemplo en el botón de búsquedas y muestra los resultados
+                }
                
             }
         )
